e80ee30447df68cb28342956f0197e6025d300d2
Carlos Carbajal <cmcarbajal@gmail.com>
2025-12-07 01:24:08 -0500

    eliminate temp files

--- temp_install.R
+++
@@ -1,9 +0,0 @@
-# Test installation from source
-if (!require('devtools', quietly = TRUE)) {
-  install.packages('devtools', repos='https://cloud.r-project.org')
-}
-
-cat("Installing soilquality package from source...\n")
-devtools::install(upgrade = "never")
-
-cat("\nInstallation completed successfully!\n")
